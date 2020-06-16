9a0424ee32038c4805c0086562008fe89b545b6c
Panviktor <panafviktor@gmail.com>
2020-06-16 15:00:52 +0300

    Initial Commit

---
+++ CupcakeCorner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CupcakeCorner
+//
+//  Created by Виктор on 16.06.2020.
+//  Copyright © 2020 SwiftViktor. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
